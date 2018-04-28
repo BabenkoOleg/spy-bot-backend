35cbec8013aab81ad9260e91739de017b0995362
Kravchuk <majerkravchuk@gmail.com>
2018-04-28 15:28:50 +0300

    Fix app booting

--- boot.rb
+++ boot.rb
@@ -2,13 +2,12 @@ require 'rubygems'
 require 'bundler/setup'
 require 'logger'
 require 'yaml'
-require 'telegram/bot'
 
 Bundler.require
 
 Dir[File.join(File.dirname(__FILE__), 'lib', '*.rb')].each { |file| require file }
 
-%w[figaro sequel clients].each { |file| require "./config/#{file}" }
+%w[figaro sequel spy_bot].each { |file| require "./config/#{file}" }
 
 Dir[File.join(File.dirname(__FILE__), 'app', 'models', '*.rb')].each { |file| require file }
-Dir[File.join(File.dirname(__FILE__), 'app', 'jobs', '*.rb')].each { |file| require file }
+Dir[File.join(File.dirname(__FILE__), 'app', 'workers', '*.rb')].each { |file| require file }
--- config.ru
+++ config.ru
@@ -1,8 +1,6 @@
 require './boot'
-require './app/api'
 require 'sidekiq/web'
 
 run Rack::URLMap.new(
-  '/' => API,
   '/sidekiq' => Sidekiq::Web
 )
